f3742b581acbe59008a5777871cf3bf8cb700e79
arolik <43118063+arolik@users.noreply.github.com>
2021-09-05 14:19:26 +0300

    reworked 2.0

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
+/*
 class CreateMessage {
     constructor() {
-        this.target = document.querySelector('#target');
         this.field = document.querySelector('#field');
         this.enterMessage = document.querySelector('#enterMessage');
         this.sendMessage = document.querySelector('#sendMessage');
-        this.id = this.createId();
 
         this.sendMessage.addEventListener('click', this.pushMessage.bind(this));
     }
@@ -12,39 +11,40 @@ class CreateMessage {
     pushMessage() {
         if (this.enterMessage.value != '') {
             let message = this.enterMessage.value;
-            list.createList(message, this.id(), this.target, this.field);
+            list.createList(message,  this.field);
             this.enterMessage.value = '';
         } else if (this.enterMessage.value == '') {
             alert('enter your message');
         }
-
     }
-    createId() {
-        let id = 0;
-        return function () {
-            id++;
-            return id;
-        }
-    }
-
 }
 
 class ListMessages {
     constructor() {
         this.parrent = document.querySelector('#listMessages');
+        this.target = document.querySelector('#target');
         this.catalogMessages = [];
         this.createList = this.makeList;
+        this.target.addEventListener('click', this.checkMessage.bind(this));
     }
-    makeList(textMessage, id, target, field) {
-
-        this.catalogMessages.push({
-            text: textMessage,
-            textId: id,
-            isShow: false
-        });
+    makeList(textMessage,  field) {
+        
+        this.catalogMessages.push(textMessage);
         let arrayMessages = this.catalogMessages;
         view.clearBlock(this.parrent);
-        view.setBlock(this.parrent, arrayMessages, target, field);
+        view.setBlock(this.parrent, arrayMessages, field);
+    }
+
+    checkMessage(event){
+        let parent = event.target.parentElement;
+        let neighbor = parent.nextElementSibling;
+        let catalog = this.catalogMessages;
+        for(let i=0; i<catalog.length; i++){
+            if(neighbor.innerText === catalog[i]){
+                catalog[i] = 'X' + catalog[i];
+            }
+        }
+        this.makeList(); // нужно передать параметры
     }
 }
 
@@ -58,16 +58,16 @@ class ViewMessages {
             parrent.removeChild(parrent.firstChild);
         }
     }
-    setField(parrent, arrayMessages, target, field) {
+    setField(parrent, arrayMessages, field) {
         console.log(arrayMessages);
         arrayMessages.forEach(function (elem) {
             let p = document.createElement('p');
-            p.innerText = elem.text;
+            p.innerText = elem;
             parrent.appendChild(p);
         });
 
         moveMessage.stop();
-        moveMessage.move(arrayMessages, target, field);
+        moveMessage.move(arrayMessages, field);
     }
 }
 
@@ -77,12 +77,11 @@ class ShowMessages {
         this.move = this.upMessage;
         this.stop = this.stopUpMessage;
     }
-    upMessage(arr, target, field) {
+    upMessage(arr, field) {
         let i = 0;
         this.interval = setInterval(function () {
             console.log(i);
-            target.checked = false;
-            field.innerText = arr[i].text;
+            field.innerText = arr[i];
             i++;
             if (i >= arr.length) {
                 i = 0;
@@ -94,8 +93,38 @@ class ShowMessages {
     }
 }
 
-
 let message = new CreateMessage();
 let list = new ListMessages();
 let view = new ViewMessages();
 let moveMessage = new ShowMessages();
+*/
+
+
+class TestApp {
+    constructor(){
+        this.target = document.querySelector('#target');
+        this.parrent = document.querySelector('#listMessages');
+        this.field = document.querySelector('#field');
+        this.enterMessage = document.querySelector('#enterMessage');
+        this.sendMessage = document.querySelector('#sendMessage');
+        this.sendMessage.addEventListener('click', this.setMessage.bind(this));
+        this.catalogMessages = [];
+    }
+
+    setMessage(){
+        if(this.enterMessage.value != ''){
+            let text = this.enterMessage.value;
+            this.catalogMessages.push(text);
+            this.enterMessage.value = '';
+            console.log(this.catalogMessages);
+        } else if(this.enterMessage.value == ''){
+            alert('enter your message');
+        }
+    }
+    
+
+}
+
+
+new TestApp();
+
