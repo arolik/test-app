4456bc176fccaca9f980dcf3e77651e8cd0b2408
arolik <43118063+arolik@users.noreply.github.com>
2021-09-05 13:40:27 +0300

    reworked

--- js/main.js
+++ js/main.js
@@ -1,45 +1,47 @@
-
 class CreateMessage {
-    constructor(){
+    constructor() {
         this.target = document.querySelector('#target');
         this.field = document.querySelector('#field');
         this.enterMessage = document.querySelector('#enterMessage');
         this.sendMessage = document.querySelector('#sendMessage');
-        this.message = '';
         this.id = this.createId();
-        
-        this.enterMessage.addEventListener('blur', this.setMessage.bind(this));
+
         this.sendMessage.addEventListener('click', this.pushMessage.bind(this));
     }
-    setMessage(event){
-        this.message = event.target.value;
+
+    pushMessage() {
+        if (this.enterMessage.value != '') {
+            let message = this.enterMessage.value;
+            list.createList(message, this.id(), this.target, this.field);
+            this.enterMessage.value = '';
+        } else if (this.enterMessage.value == '') {
+            alert('enter your message');
+        }
+
     }
-    pushMessage(){
-        list.createList(this.message, this.id(), this.target, this.field, this.target.value,);
-    }
-    createId(){
+    createId() {
         let id = 0;
-        return function(){
+        return function () {
             id++;
             return id;
         }
     }
-    
 
-    
 }
 
 class ListMessages {
-    constructor(){
+    constructor() {
         this.parrent = document.querySelector('#listMessages');
         this.catalogMessages = [];
         this.createList = this.makeList;
     }
-    makeList(textMessage, id, target, field, targetValue){
-        
-        
+    makeList(textMessage, id, target, field) {
 
-        this.catalogMessages.push({text:textMessage, textId:id, isShow:false});
+        this.catalogMessages.push({
+            text: textMessage,
+            textId: id,
+            isShow: false
+        });
         let arrayMessages = this.catalogMessages;
         view.clearBlock(this.parrent);
         view.setBlock(this.parrent, arrayMessages, target, field);
@@ -47,51 +49,49 @@ class ListMessages {
 }
 
 class ViewMessages {
-    constructor(){
+    constructor() {
         this.clearBlock = this.clearField;
         this.setBlock = this.setField;
     }
-    clearField(parrent){
-        while(parrent.children.length > 0){
+    clearField(parrent) {
+        while (parrent.children.length > 0) {
             parrent.removeChild(parrent.firstChild);
         }
     }
-    setField(parrent, arrayMessages, target, field){
+    setField(parrent, arrayMessages, target, field) {
         console.log(arrayMessages);
-        arrayMessages.forEach(function(elem){
+        arrayMessages.forEach(function (elem) {
             let p = document.createElement('p');
             p.innerText = elem.text;
             parrent.appendChild(p);
         });
-        
+
         moveMessage.stop();
         moveMessage.move(arrayMessages, target, field);
     }
 }
 
 class ShowMessages {
-    constructor(){
+    constructor() {
         this.interval;
-        this.move = this.upMessage; 
-        this.stop = this.stopUpMessage;       
+        this.move = this.upMessage;
+        this.stop = this.stopUpMessage;
     }
-    upMessage(arr, target, field){
+    upMessage(arr, target, field) {
         let i = 0;
-        this.interval = setInterval(function(){
-        console.log(i);
-        target.value = arr[i].textId;
-        target.checked = false;
-        field.innerText = arr[i].text;
-        i++;
-        if(i >= arr.length){
-            i = 0;
-        }
+        this.interval = setInterval(function () {
+            console.log(i);
+            target.checked = false;
+            field.innerText = arr[i].text;
+            i++;
+            if (i >= arr.length) {
+                i = 0;
+            }
         }, 1000);
     }
-    stopUpMessage(){
+    stopUpMessage() {
         clearInterval(this.interval);
     }
-    
 }
 
 
